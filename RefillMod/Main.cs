86ef1e8e149f8d8ec01331ad2ccfe6cfc2bc3b2d
Andrey Onishchenko <loraner123@gmail.com>
2022-06-24 16:47:33 +0300

    Initial commit

---
+++ RefillMod/Main.cs
@@ -0,0 +1,25 @@
+﻿using ModLoader;
+
+namespace RefillMod
+{
+    public class Main : Mod
+    {
+        public Main() : base("RefillMod", "RefillMod", "Andrey Onishchenko",
+            "0.5.7", "v1.0", "Example mod") { }
+
+        public override void Early_Load()
+        {
+            //
+        }
+
+        public override void Load()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Unload()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
